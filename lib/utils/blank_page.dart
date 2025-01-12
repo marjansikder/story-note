230114ad32a8c0559881456710b51e195d6c8ac3
Marjan Sikder <marjan@squarehealth.com.bd>
2025-01-12 17:19:24 +0600

    Ui update

---
+++ lib/utils/blank_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class BlankPage extends StatelessWidget {
+  final Widget children;
+  final EdgeInsetsGeometry? margin;
+  final EdgeInsetsGeometry? padding;
+
+  const BlankPage({super.key, required this.children, this.margin, this.padding});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: margin,
+      padding: padding,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+        image: const DecorationImage(
+          image: AssetImage("assets/images/note_list.png"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: children,
+    );
+  }
+}
